68e97ad131705e8f0c184a98d4fcb97c0450bbd8
GR01\chris <christopher.talke@gmail.com>
2018-03-16 07:51:16 +1100

    Login & Conditional on Home.ejs
    - Added Login and Registration
    - Added condiitional to Home.EJS passed on res.param (url) conditions.

--- index.js
+++ index.js
@@ -21,25 +21,46 @@ app.set("view engine", "ejs");
 //   Routes   //
 //------------//
 
+// Serve landing page
 app.get("/", function(req, res) {
   res.render("landingpage");
 });
 
+// Serve all events
 app.get("/home", function(req, res) {
+  let parkID = null;
+
   request("http://api.jsonbin.io/b/5aa97ecb7417a517644f6bc9", function(
     error,
     response,
     body
   ) {
     let parsedBody = JSON.parse(body);
-    res.render("home", { db: parsedBody });
+    res.render("home", { db: parsedBody, parkID: parkID });
   });
 });
 
+// Serve individual event
+app.get("/home/:parkid", function(req, res) {
+  let parkID = req.params.parkid;
+
+  request("http://api.jsonbin.io/b/5aa97ecb7417a517644f6bc9", function(
+    error,
+    response,
+    body
+  ) {
+    let parsedBody = JSON.parse(body);
+    console.log(parkID);
+    res.render("home", { db: parsedBody, parkID: parkID });
+  });
+});
+
+// Serve login page
 app.get("/login", function(req, res) {
   res.render("login");
 });
 
+// Serve register page
 app.get("/register", function(req, res) {
   res.render("register");
 });
