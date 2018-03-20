9dc2f347dbb257be516ee297c518ba5d0f2e6090
C3GROUP\christopher.talke <christopher.talke@gmail.com>
2018-03-20 17:52:25 +1100

    Remove Ability (Dev Stage Only)

--- index.js
+++ index.js
@@ -32,36 +32,37 @@ function buildObj(obj1, obj2) {
 
 // This requests data from our temp database and allows us to auth
 let data = {
-  url: "http://api.jsonbin.io/b/5aaf96abffba176106431d68/latest",
+  url: "https://api.jsonbin.io/b/5ab0a71c5d1dee610789018d/latest",
   headers: {
     "secret-key": "$2a$10$E7/AKiObcqEM6M4RJl6HeOL7iFL3Qc3eUJ1uNT2cL0D2njNI.8RcG"
   }
 };
 
-
-request(data, function (error, response, body) {
+request(data, function(error, response, body) {
   parsedBody = JSON.parse(body);
 });
 
 // This parses all form data, which can then be accesed via 'apt.post' routes
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 
 //------------//
 //   Routes   //
 //------------//
 
 // Serve home page and all events, this is the home page and will serve all event data
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.render("home", {
     db: parsedBody
   });
 });
 
 // Serve individual event, this will serve detailed data about a selected event
-app.get("/home/:parkid", function (req, res) {
+app.get("/view/:parkid", function(req, res) {
   let parkID = req.params.parkid;
   res.render("detailed", {
     db: parsedBody,
@@ -70,17 +71,17 @@ app.get("/home/:parkid", function (req, res) {
 });
 
 // Serve login page, this will allow a user to login or auth to the app
-app.get("/login", function (req, res) {
+app.get("/login", function(req, res) {
   res.render("login");
 });
 
 // Serve register page, this page will allow a user to register to the app
-app.get("/register", function (req, res) {
+app.get("/register", function(req, res) {
   res.render("register");
 });
 
 // Serve addEvent page, this page will allow a user to input a new event
-app.get("/addEvent", function (req, res) {
+app.get("/addEvent", function(req, res) {
   res.render("addEvent");
 });
 
@@ -89,39 +90,66 @@ app.get("/addEvent", function (req, res) {
 // --------------//
 
 // Serve addEvent page
-app.post("/addEvent/submit", function (req, res) {
-
-  // Produce a new ID for the JSON Object
-  let newNum = Object.keys(parsedBody.events).length + 1;
-
-  // Pass in the new JSON Object & turn to string
-  parsedBody.events[newNum] = req.body;
+app.post("/addEvent/submit", function(req, res) {
+  //Push New Object
+  parsedBody.events.push(req.body);
   let formData = JSON.stringify(parsedBody);
 
   // Send a put request to JSON Bin
-  request.put({
-      url: "http://api.jsonbin.io/b/5aaf96abffba176106431d68",
+  request.put(
+    {
+      url: "https://api.jsonbin.io/b/5ab0a71c5d1dee610789018d",
       headers: {
-        "secret-key": '$2a$10$E7/AKiObcqEM6M4RJl6HeOL7iFL3Qc3eUJ1uNT2cL0D2njNI.8RcG',
-        "Content-type": 'application/json'
+        "secret-key":
+          "$2a$10$E7/AKiObcqEM6M4RJl6HeOL7iFL3Qc3eUJ1uNT2cL0D2njNI.8RcG",
+        "Content-type": "application/json"
       },
-      body: formData,
+      body: formData
     },
     function optionalCallback(err, httpResponse, body) {
       if (err) {
-        return console.error('upload failed:', err);
+        return console.error("upload failed:", err);
       }
-      console.log('Post Completed, Event: \"' + parsedBody.events[newNum].name + '\" has been posted to the DB!');
-    });
+      console.log("Post Completed, Event has been posted to the DB!");
+    }
+  );
 
   res.redirect("/");
+});
 
+app.get("/delete/:pid", function(req, res) {
+  let pID = req.params.pid;
+
+  console.log("Event " + parsedBody.events[pID].name + " has been removed!");
+  parsedBody.events.splice(pID, pID++);
+  let formData = JSON.stringify(parsedBody);
+
+  // Send a put request to JSON Bin
+  request.put(
+    {
+      url: "https://api.jsonbin.io/b/5ab0a71c5d1dee610789018d",
+      headers: {
+        "secret-key":
+          "$2a$10$E7/AKiObcqEM6M4RJl6HeOL7iFL3Qc3eUJ1uNT2cL0D2njNI.8RcG",
+        "Content-type": "application/json"
+      },
+      body: formData
+    },
+    function optionalCallback(err, httpResponse, body) {
+      if (err) {
+        return console.error("upload failed:", err);
+      }
+      console.log("Post Completed, Event has been removed from the DB!");
+    }
+  );
+
+  res.redirect("/");
 });
 
 //--------------//
 //  Initialiser //
 //--------------//
 
-app.listen(3000, function () {
+app.listen(3000, function() {
   console.log("The application is being served on http://localhost:3000");
 });
