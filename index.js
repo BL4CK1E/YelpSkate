2084723c70440125fa4f408b99ba87600f5c0c53
C3GROUP\christopher.talke <christopher.talke@gmail.com>
2018-03-20 14:47:41 +1100

    Changes
    Changes:
    - Added further Event Adding features
    - Added ability to add events to the 'database', however, this is only temp and will reset once the NodeJS instance is reset.
    - Need to figure out how to 'PUT' this to the JSONbin.io data, alternatively we can move this to use MongoDB as per the WebDev Bootcamp.

--- index.js
+++ index.js
@@ -4,68 +4,101 @@
 
 const express = require("express"),
   app = express();
+const bodyParser = require("body-parser");
 const request = require("request");
 const path = require("path");
+const $ = require("jQuery");
 
-//-------------//
-//   Servers   //
-//-------------//
+//-------------------//
+//   Static Serves   //
+//-------------------//
 
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
+
+//---------------------//
+//   Cust. Functions   //
+//---------------------//
+
+function buildObj(obj1, obj2) {
+  obj1 = '"' + obj1.toString() + '":';
+  obj2 = JSON.stringify(obj2);
+  builtObj = "{" + obj1 + obj2 + "}";
+}
 
 //-------------//
 //     Data    //
 //-------------//
 
-var data = {
+// This requests data from our temp database and allows us to auth
+let data = {
   url: "http://api.jsonbin.io/b/5aaf96abffba176106431d68/latest",
   headers: {
     "secret-key": "$2a$10$E7/AKiObcqEM6M4RJl6HeOL7iFL3Qc3eUJ1uNT2cL0D2njNI.8RcG"
   }
 };
 
+
+request(data, function (error, response, body) {
+  parsedBody = JSON.parse(body);
+});
+
+// This parses all form data, which can then be accesed via 'apt.post' routes
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
 //------------//
 //   Routes   //
 //------------//
 
-// Serve home page and all events
-app.get("/", function(req, res) {
-  request(data, function(error, response, body) {
-    let parsedBody = JSON.parse(body);
-    res.render("home", { db: parsedBody });
+// Serve home page and all events, this is the home page and will serve all event data
+app.get("/", function (req, res) {
+  res.render("home", {
+    db: parsedBody
   });
 });
 
-// Serve individual event
-app.get("/home/:parkid", function(req, res) {
+// Serve individual event, this will serve detailed data about a selected event
+app.get("/home/:parkid", function (req, res) {
   let parkID = req.params.parkid;
-
-  request(data, function(error, response, body) {
-    let parsedBody = JSON.parse(body);
-    res.render("detailed", { db: parsedBody, pID: parkID });
+  res.render("detailed", {
+    db: parsedBody,
+    pID: parkID
   });
 });
 
-// Serve login page
-app.get("/login", function(req, res) {
+// Serve login page, this will allow a user to login or auth to the app
+app.get("/login", function (req, res) {
   res.render("login");
 });
 
-// Serve register page
-app.get("/register", function(req, res) {
+// Serve register page, this page will allow a user to register to the app
+app.get("/register", function (req, res) {
   res.render("register");
 });
 
-// Serve addEvent page
-app.get("/addEvent", function(req, res) {
+// Serve addEvent page, this page will allow a user to input a new event
+app.get("/addEvent", function (req, res) {
   res.render("addEvent");
+});
+
+// --------------//
+//   POST/PUT   //
+// --------------//
+
+// Serve addEvent page
+app.post("/addEvent/submit", function (req, res) {
+  let newNum = Object.keys(parsedBody.events).length + 1;
+  parsedBody.events[newNum] = req.body;
+  res.redirect("/");
 });
 
 //--------------//
 //  Initialiser //
 //--------------//
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("The application is being served on http://localhost:3000");
 });
