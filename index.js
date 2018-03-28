2f8c81e2cc83b923828595ca74145a2b93e9ff90
GR01\chris <christopher.talke@gmail.com>
2018-03-29 07:43:34 +1100

    Database Change:
    - Changed from JSONBin to MongoDB/Mongoose
    - View All from DB
    - View Individual from DB
    - Add to DB
    - Remove from DB
    - Edit Individual and save to DB

--- index.js
+++ index.js
@@ -44,35 +44,6 @@ let eventSchema = new mongoose.Schema({
 
 let Event = mongoose.model("Event", eventSchema);
 
-// Event.create(
-//   {
-//     name: "Bowlarama",
-//     city: "Sydney",
-//     state: "NSW",
-//     streetNumber: "Bondi",
-//     eventDate: "25th Apr 2018",
-//     shortOverview:
-//       "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum ",
-//     picture:
-//       "https://skateboarding.transworld.net/wp-content/blogs.dir/440/files/2012/02/Vans-Bowl-Artwork-_FINAL-424x600.jpg"
-//   },
-// function(err, event) {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log(event);
-//   }
-// }
-// );
-let name = "Harbour";
-Event.find({ city: { $regex: name } }, function(err, event) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(event);
-  }
-});
-
 //------------------//
 //     Form Data    //
 //------------------//
@@ -91,17 +62,29 @@ app.use(
 
 // Serve home page and all events, this is the home page and will serve all event data
 app.get("/", function(req, res) {
-  res.render("home", {
-    db: parsedBody
+  Event.find({}, function(err, event) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("home", {
+        db: event
+      });
+    }
   });
 });
 
 // Serve individual event, this will serve detailed data about a selected event
 app.get("/view/:parkid", function(req, res) {
   let parkID = req.params.parkid;
-  res.render("detailed", {
-    db: parsedBody,
-    pID: parkID
+
+  Event.findOne({ _id: parkID }, function(err, event) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("detailed", {
+        db: event
+      });
+    }
   });
 });
 
@@ -122,66 +105,63 @@ app.get("/add", function(req, res) {
 
 // Serve addEvent page
 app.post("/add/submit", function(req, res) {
-  //Push New Object
-  parsedBody.events.push(req.body);
-  let formData = JSON.stringify(parsedBody);
-
-  // Send a put request to JSON Bin
-  request.put(
-    {
-      url: "https://api.jsonbin.io/b/5ab0a71c5d1dee610789018d",
-      headers: {
-        "secret-key":
-          "$2a$10$E7/AKiObcqEM6M4RJl6HeOL7iFL3Qc3eUJ1uNT2cL0D2njNI.8RcG",
-        "Content-type": "application/json"
-      },
-      body: formData
-    },
-    function optionalCallback(err, httpResponse, body) {
-      if (err) {
-        return console.error("upload failed:", err);
-      }
-      console.log("Post completed, event added!");
+  Event.create(req.body, function(err, event) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("---> Added Event:'" + req.body.name + "' to the database!");
+      res.redirect("/");
     }
-  );
+  });
+});
 
-  res.redirect("/");
+// Serve editEvent page
+app.get("/edit/:pid", function(req, res) {
+  let pID = req.params.pid;
+
+  Event.findOne({ _id: pID }, function(err, event) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("editEvent", {
+        db: event
+      });
+    }
+  });
+});
+
+app.post("/edit/submit/:pid", function(req, res) {
+  let pID = req.params.pid;
+
+  Event.findOne({ _id: pID }, function(err, event) {
+    if (err) {
+      console.log(err);
+    } else {
+      event.set(req.body);
+      event.save(function(err, updatedEvent) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("---> Made changes to:'" + pID + "' to the database!");
+        }
+      });
+      res.redirect("/");
+    }
+  });
 });
 
 // Remove Event Route
 app.get("/delete/:pid", function(req, res) {
   let pID = req.params.pid;
 
-  // If first of array, delete otherwhise splice the object
-  if (pID < 1) {
-    parsedBody.events.shift();
-    console.log(parsedBody.events);
-  } else {
-    parsedBody.events.splice(pID, pID++);
-  }
-  let formData = JSON.stringify(parsedBody);
-
-  // Send new data to JSON Bin
-  request.put(
-    {
-      url: "https://api.jsonbin.io/b/5ab0a71c5d1dee610789018d",
-      headers: {
-        "secret-key":
-          "$2a$10$E7/AKiObcqEM6M4RJl6HeOL7iFL3Qc3eUJ1uNT2cL0D2njNI.8RcG",
-        "Content-type": "application/json"
-      },
-      body: formData
-    },
-    function optionalCallback(err, httpResponse, body) {
-      if (err) {
-        return console.error("upload failed:", err);
-      }
-      console.log("Post completed, event removed!");
+  Event.remove({ _id: pID }, function(err, event) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('---> Removed ID: "' + pID + '" from the database!');
+      res.redirect("/");
     }
-  );
-
-  // Send user back to the home page
-  res.redirect("/");
+  });
 });
 
 // Catch All
