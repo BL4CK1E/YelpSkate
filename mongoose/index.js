7ee8d20c7f6c949cee15ad83955d64971c8a8f08
Christopher Talke <christopher.talke@gmail.com>
2018-05-18 22:23:41 +1000

    V2
    test

--- mongoose/index.js
+++ mongoose/index.js
@@ -4,5 +4,6 @@ const dbuser = "evntr_db";
 const dbpass = "talke123";
 
 mongoose.connect(
-  "mongodb://" + dbuser + ":" + dbpass + "@ds125469.mlab.com:25469/evntr"
+  //"mongodb://" + dbuser + ":" + dbpass + "@ds125469.mlab.com:25469/evntr"
+  "mongodb://localhost:27017/evntr"
 );
